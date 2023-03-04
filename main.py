75e6530e13e8505975f119b73fdac36f0b6d8469
nazmisyed <105331233+nazmi-tarmizi-knb@users.noreply.github.com>
2023-03-04 20:51:47 +0800

    change to make things simpler

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 import os
 import openai
 import uuid
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
+from typing import List, Dict
+import json
+import requests
+
+
+##todo
+#reset 
+#migrate to azure functions or aws equivalent
+
 
 try:
-    #config file contains env var to test in local also contains credentials
+    #config file contains env var to test in local 
     # if u clone directly from github, there is no config file there. please ask the dev.
     
     from config import load_env_var #in gitignore
@@ -14,54 +26,82 @@ try:
 except:
     print("In Cloud or missing config.py file",flush=True)
 
-app = FastAPI()
+## initialization-----------------------
+FRONT_END_URL = os.environ["FRONT_END_URL"]
 
-class ChatMessage(BaseModel):
-    content: str
+origins = [
+FRONT_END_URL
+]
+
+# here we using starllette middleware instead of FastAPI built in  middleware because of bug
+middleware = [
+    Middleware(CORSMiddleware, allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"])
+]
+
+app = FastAPI(middleware=middleware)
+
+
+class Data(BaseModel):
+    role:str
+    content:str
+
+class ChatItem(BaseModel):
+    id:int
+    data:Data
+
+class ChatItems(BaseModel):
+    message:List[ChatItem] #not really sure here
+    key:str
 
 class Chat:
-    def __init__(self, title):
-        self.title = title
+    def __init__(self,key):
+        self.key = key
         self.id = str(uuid.uuid4())
-        self.system_prompt = "You are a helpful assistant."
-        self.messages = [{"id":0,"data":{"role":"system","content": "You are a helpful assistant."}}]
-        self.__auth()
         self.total_token = 0
 
-    def __auth(self):
-        openai.api_key = os.environ["OPENAI_API_KEY"]
-    
-    def __add_id(self):
-        return len(self.messages)
-
-    def __get_data_values(self):
-        return [message['data'] for message in self.messages]
-
-    def __get_openai_reply(self):
+    def __get_openai_reply(self,whole_messages):
         try:
-            completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=self.__get_data_values())
+            url = "https://api.openai.com/v1/chat/completions"
+
+            payload = json.dumps({
+                "model": "gpt-3.5-turbo",
+                "messages": jsonable_encoder([message.data for message in whole_messages])
+            })
+            headers = {
+                'Content-Type': 'application/json',
+                'Authorization': f'Bearer {self.key}'
+            }
+
+            completion= requests.request("POST", url, headers=headers, data=payload).json()
             answer = completion["choices"][0]["message"]["content"]
             self.total_token = completion["usage"]["total_tokens"]
-            print(answer)
+            
         except Exception as err:
             print(f"Error getting OpenAI reply: {err}")
-            answer = "Sorry, Internal Server Issue"
+            answer = f"Sorry, Internal Server Issue: {err}"
        
         return answer
 
-    def add_message(self, content):
-        message = {"id":self.__add_id(),"data":{"role": "user", "content": content}}
-        self.messages.append(message) 
-        reply = {"id":self.__add_id(),"data":{"role": "assistant", "content": self.__get_openai_reply()}}
-        self.messages.append(reply)
 
-chat = Chat("My Chat")
+    def send_messages(self, whole_messages):
+        """
+        whole_messages is Array of Dicts
+        """
+        
+        reply = {"id":len(whole_messages),"data":{"role": "assistant", "content": self.__get_openai_reply(whole_messages)}} #will wait until get reply
 
-@app.post("/message")
-async def add_message(message: ChatMessage):
-    chat.add_message(message.content)
-    return {"message": "Message added successfully."}
+        return reply
 
-@app.get("/messages")
-async def get_messages():
-    return {"messages": chat.messages}
+
+
+@app.post("/message2")
+async def send_messages(messages: ChatItems):
+    ##message is array of dictionaries
+    
+    chat = Chat(key=messages.key)
+    response = chat.send_messages(whole_messages=messages.message)
+    return {"message": response} # return back the reply only
+
